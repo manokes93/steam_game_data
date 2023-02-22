6f9c516bcaae8dd8dc6bb97b43b4a85c1d374dfb
Michael Nokes <manokes93@gmail.com>
2023-02-21 23:41:34 -0500

    Change requests.get to be a Session and set up multithreading for the game details function. Takes about 6 secs to run now.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
 import creds
 import pandas as pd
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from concurrent.futures import ThreadPoolExecutor
 
-def myGames():
+
+# url = f'http://store.steampowered.com/api/appdetails?appids={976310}'
+# response = requests.get(url)
+# json = response.json()
+# print(json)
+
+def my_games():
     my_steamid = '76561198070917858'
 
     my_owned_games = requests.get(f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={creds.api_key}&steamid={my_steamid}&format=json').json()
@@ -22,10 +31,12 @@ def myGames():
 
     df = pd.DataFrame(rows)
     df['appid'] = df['appid'].astype(str)
+    print('Created myGames df')
     return df
 
-def gameDetails():
-    games = myGames()['appid']
+
+def game_details():
+    games = my_games()['appid']
 
     details_rows = {
         'appid': [],
@@ -34,9 +45,13 @@ def gameDetails():
         'metacritic_score': []
         }
 
-    for app in games:
+    session = requests.Session()
+    retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+    session.mount('http://', HTTPAdapter(max_retries=retries))
+
+    def process_app(app):
         url = f'http://store.steampowered.com/api/appdetails?appids={app}'
-        response = requests.get(url)
+        response = session.get(url)
         json = response.json()
 
         details_rows['appid'].append(app)
@@ -64,23 +79,32 @@ def gameDetails():
                 details_rows['metacritic_score'].append(None)
         except KeyError:
             details_rows['metacritic_score'].append(None)
+        print('Row added for ' + app)
+
+    # Use ThreadPoolExecutor to make parallel requests
+    with ThreadPoolExecutor(max_workers=12) as executor:
+        executor.map(process_app, games)
 
     details_df = pd.DataFrame(details_rows)
-
+    print('Game Details df created.')
     return details_df
 
+
 def merge():
-    right_df = gameDetails()
-    left_df = myGames()
+    right_df = game_details()
+    left_df = my_games()
 
     merged_df = left_df.merge(right_df, on='appid', how='left')
 
+    print('Merged both dfs')
     return merged_df
+
 
 def load():
     final_df = merge()
 
     final_df.to_csv('steam_game_details.csv', header=True)
+
 
 if __name__ == '__main__':
     load()
